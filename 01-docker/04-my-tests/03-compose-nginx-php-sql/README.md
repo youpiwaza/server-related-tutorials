ff71eedf5450a49639c04d8bfe78483d04d59066
Max <masamune.code@gmail.com>
2020-01-05 15:16:48 +0100

    🎉ADD: Initial commit, w README  & docker compose nginx php

---
+++ 01-docker/04-my-tests/03-compose-nginx-php-sql/README.md
@@ -0,0 +1,51 @@
+# Monter un compose Nginx, PHP, SQL & Admin
+
+Pour tester : `docker-compose up`
+
+Sources :
+
+- *Fichier docker-compose.yml*
+- *Dossier sources /src*
+- *Dossier configs /config*
+  - Contient également les configurations par défaut des deux images
+
+On repart du compose nginx + php :)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
---
+++ 01-docker/04-my-tests/03-compose-nginx-php-sql/src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Test PHP Mysql</title>
+</head>
+<body>
+	<h1>Test PHP & MySQL</h1>
+	<p>Test de récupération SQL via PHP : <?php echo 'Fuck yeah'; ?></p>
+	<p><a href="todo.php">Administration SQL</a></p>
+</body>
+</html>
